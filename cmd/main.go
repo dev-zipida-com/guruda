894ba092752af6570844e73f3e652ed903d8d14d
eatingbug <sj.hong@zipida.com>
2023-04-07 11:48:52 +0900

    Add Github API token

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/dev-zipida-com/guruda/pkg"
 	"github.com/google/go-github/v50/github"
+	"golang.org/x/oauth2"
 )
 
 func main() {
@@ -15,13 +18,19 @@ func main() {
 	fmt.Print("Enter GitHub repository name: ")
 	fmt.Scanf("%s", &reponame)
 
-	client := github.NewClient(nil)
+	token := os.Getenv("GITHUB_TOKEN")
+
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
 
 	_, err := pkg.FetchContentsRecursively(client, username, reponame, "")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("FetchContentsRecursively error: ", err)
 		return
 	}
-
-	
 }
