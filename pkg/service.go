acbe02d57b94cc126fa607dca5bda82d02066202
eatingbug <sj.hong@zipida.com>
2023-04-07 15:19:46 +0900

    Refactor import ModulesList

--- pkg/service.go
+++ pkg/service.go
@@ -3,21 +3,21 @@ package pkg
 import (
 	"encoding/base64"
 	"encoding/json"
+	"go/parser"
+	"go/token"
 	"net/http"
+	"path/filepath"
 
 	"github.com/google/go-github/v50/github"
 
 	"context"
 	"fmt"
-	"log"
 	"os"
 	"regexp"
 	"strings"
 
-	"github.com/joho/godotenv"
 	openai "github.com/sashabaranov/go-openai"
 )
-
 
 type Response struct {
 	Name        string `json:"name"`
@@ -38,33 +38,28 @@ type Response struct {
 }
 
 type Box struct {
-	Content string `json:"content"`
-	FilePath string `json:"filePath"`
-	Modules []string `json:"modules"`
-	ProcessedContent string `json:"processedContent"`
+	Content          string   `json:"content"`
+	FilePath         string   `json:"filePath"`
+	Modules          []string `json:"modules"`
+	ProcessedContent string   `json:"processedContent"`
 }
 
-func GetContent(url string) string {
-	// err := godotenv.Load("../.env")
-	// if err != nil {
-    //     log.Fatal("Error loading .env file")
-    // }
+func GetContent(url string) (string, error) {
 
 	method := "GET"
+	token := os.Getenv("GITHUB_TOKEN")
 
 	client := &http.Client{}
 	req, err := http.NewRequest(method, url, nil)
-	req.Header.Set("Authorization", "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")
+	req.Header.Set("Authorization", "token "+token)
 
 	if err != nil {
-		fmt.Println(err)
-		return ""
+		return "", err
 	}
 
 	res, err := client.Do(req)
 	if err != nil {
-		fmt.Println(err)
-		return ""
+		return "", err
 	}
 	defer res.Body.Close()
 
@@ -72,52 +67,71 @@ func GetContent(url string) string {
 
 	err = json.NewDecoder(res.Body).Decode(&resContent)
 	if err != nil {
-		fmt.Println(err)
-		return ""
+		return "", err
 	}
 
 	decodedBytes, err := base64.StdEncoding.DecodeString(resContent.Content)
 	if err != nil {
-		fmt.Println(err)
-		return ""
+		return "", err
 	}
 
-	return string(decodedBytes)
+	if string(decodedBytes) == "" {
+		return "", fmt.Errorf("Content Not Found")
+	}
+
+	return string(decodedBytes), nil
+}
+
+func GetImportedModulesListInGolang(content string) ([]string, error) {
+	// 문자열을 파일로 변환
+	fset := token.NewFileSet()
+	file, err := parser.ParseFile(fset, "", content, parser.AllErrors)
+	if err != nil {
+		return nil, err
+	}
+
+	// import 문에서 사용된 패키지 추출
+	var packages []string
+	for _, i := range file.Imports {
+		path := i.Path.Value[1 : len(i.Path.Value)-1]
+		packages = append(packages, path)
+	}
+
+	// 추출된 패키지 리스트 출력
+	return packages, nil
 }
 
 func GetImportedModulesList(filePath string, content string) ([]string, error) {
-	splitedNamebySlash := strings.Split(filePath, "/")
-	fileName := splitedNamebySlash[len(splitedNamebySlash)-1]
-	extensions := strings.Split(fileName, ".")
-	extensionName := extensions[len(extensions)-1]
 	var importRegex *regexp.Regexp
+	fileExtension := filepath.Ext(filePath)
 
-	if(extensionName == "go") {
-		importRegex = regexp.MustCompile(`^\s*import\s+(?:.+\s+)?["'](.+)["']`)
-	} else if (extensionName == "py") {
+	switch fileExtension {
+	case ".go":
+		return GetImportedModulesListInGolang(content)
+	case ".py":
 		importRegex = regexp.MustCompile(`^\s*import\s+(\w+|\.)+\s*$`)
-	} else if (extensionName == "java") {
+	case ".java":
 		importRegex = regexp.MustCompile(`^\s*import\s+(?:static\s+)?([\w\.]+)\s*;?$`)
-	} else if (extensionName == "ts" || extensionName == "js") {
+	case ".ts", ".js":
 		importRegex = regexp.MustCompile(`import\s+(?:.+\s+from\s+)?['"](.+)['"]`)
-	} else {
-		return make([]string, 1), fmt.Errorf("invalid file extension")
+	default:
+		return nil, fmt.Errorf("invalid file extension: %s", fileExtension)
+	}
+
+	if content == "" {
+		return nil, fmt.Errorf("content not found")
 	}
 
 	matches := importRegex.FindAllStringSubmatch(content, -1)
-    modules := make([]string, len(matches))
-    for i, match := range matches {
-        modules[i] = match[1]
-    }
+	modules := make([]string, len(matches))
+	for i, match := range matches {
+		modules[i] = match[1]
+	}
 
-    return modules, nil
+	return modules, nil
 }
 
 func getResponseFromChatGPT(message string) (string, error) {
-	err := godotenv.Load("../.env")
-	if err != nil {
-        log.Fatal("Error loading .env file")
-    }
 
 	client := openai.NewClient(os.Getenv("OPENAI_API_KEY"))
 
@@ -140,20 +154,19 @@ func getResponseFromChatGPT(message string) (string, error) {
 		fmt.Printf("ChatCompletion error: %v\n", err)
 		return "", err
 	}
-	
+
 	return resp.Choices[0].Message.Content, nil
 }
 
-
 func FetchContentsRecursively(client *github.Client, owner, repo, path string) ([]Box, error) {
 	_, directoryContent, _, err := client.Repositories.GetContents(context.Background(), owner, repo, path, nil)
-	
+
 	if err != nil {
 		return nil, err
 	}
 
 	var boxes = []Box{}
-	
+
 	for _, content := range directoryContent {
 		if *content.Type == "dir" {
 			subBoxes, err := FetchContentsRecursively(client, owner, repo, *content.Path)
@@ -169,10 +182,13 @@ func FetchContentsRecursively(client *github.Client, owner, repo, path string) (
 			if len(extension) == 1 {
 				continue
 			} else {
-				extensionName = extension[len(extension) - 1]
+				extensionName = extension[len(extension)-1]
 			}
 			if extensionName == "js" || extensionName == "ts" || extensionName == "py" || extensionName == "go" || extensionName == "java" {
-				myContent := GetContent(*content.URL)
+				myContent, err := GetContent(*content.URL)
+				if err != nil {
+					fmt.Println(err)
+				}
 				myFilePath := *content.Path
 				myModules, err := GetImportedModulesList(myFilePath, myContent)
 				if err != nil {
@@ -180,12 +196,12 @@ func FetchContentsRecursively(client *github.Client, owner, repo, path string) (
 				}
 
 				box := Box{
-					Content: myContent,
-					FilePath: myFilePath,
-					Modules: myModules,
+					Content:          myContent,
+					FilePath:         myFilePath,
+					Modules:          myModules,
 					ProcessedContent: "",
 				}
-				
+				fmt.Println(box.FilePath, box.Modules)
 				boxes = append(boxes, box)
 			}
 		}
