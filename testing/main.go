92be04c97bf2fa99a42f4fa468a54c8c5b61166d
eatingbug <sj.hong@zipida.com>
2023-04-19 18:43:49 +0900

    Testing: GPT Response concurrency and edit Prompt

--- testing/main.go
+++ testing/main.go
@@ -46,7 +46,7 @@ func splitCode(s string) []string {
 	strLen := len(s)
 
 	splitedText := []string{}
-	pageNum := int(math.Ceil(float64(strLen)/float64(maxWordLen)))
+	pageNum := int(math.Ceil(float64(strLen) / float64(maxWordLen)))
 	interval := 50
 
 	startIndex := 0
@@ -59,7 +59,7 @@ func splitCode(s string) []string {
 				endIndex = maxWordLen + interval
 			} else if i == pageNum-1 {
 				startIndex = i*maxWordLen - interval
-				endIndex = strLen 
+				endIndex = strLen
 			} else {
 				startIndex = i*maxWordLen - interval
 				endIndex = (i+1)*maxWordLen + interval
@@ -77,49 +77,96 @@ func splitCode(s string) []string {
 
 func main() {
 	lang := "golang"
-	prompt := `I want you to act as a code analyzer and generate only a one JSON object of all the functions, modules, and methods used in the given code. The code is written in ` + lang + ` and is a part of a larger program. The code is given below:
-	
+	prompt := `
+	I want you to act as a code analyzer and generate only a one JSON object of all the functions, modules, and methods used in the given code. 
+	The code is written in ` + lang + ` and is a part of a larger program. The code is given below:
+	Just send me a JSON response.
+	Don't respond if the provided code snippet is incomplete
+	for example
+	{
+		"functions": [
+		  "handleSearchSubmit",
+		  "handleInputChange"
+		],
+		"modules": [
+		  "react",
+		  "react-dom",
+		  "axios"
+		],
+		"methods": [
+		  "useState",
+		  "useEffect"
+		]
+	}
+	--------------------
 	`
 	token := "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"
 
-    ctx := context.Background()
-    ts := oauth2.StaticTokenSource(
-        &oauth2.Token{AccessToken: token},
-    )
-    tc := oauth2.NewClient(ctx, ts)
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
 
-    client := github.NewClient(tc)
+	client := github.NewClient(tc)
 
-    owner := "dev-zipida-com"
-    repo := "ogada"
-    ref := "deploy"
-    path := "/frontend/src/pages/components/Address_PR.js"
+	owner := "dev-zipida-com"
+	repo := "ogada"
+	ref := "deploy"
+	path := "/frontend/src/pages/components/Address_PR.js"
 
-    file, _, _, err := client.Repositories.GetContents(ctx, owner, repo, path, &github.RepositoryContentGetOptions{
-        Ref: ref,
-    })
+	file, _, _, err := client.Repositories.GetContents(ctx, owner, repo, path, &github.RepositoryContentGetOptions{
+		Ref: ref,
+	})
 
-    if err != nil {
-        fmt.Printf("Error: %v\n", err)
-        os.Exit(1)
-    }
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
 	content, err := file.GetContent()
-    if err != nil {
-        fmt.Printf("Error: %v\n", err)
-        return
-    }
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return
+	}
 
 	code := content
 
 	splitedText := splitCode(code)
 
-	for _, text := range splitedText {
-		res, err := GetResponse(prompt, text)
-		if err != nil {
-			fmt.Println("ERROR: ")
-			fmt.Println(err)
-		}
+	// channel to receive response and order number
+	responseChan := make(chan struct {
+		order    int
+		response string
+	})
 
+	for i, text := range splitedText {
+		// start a new goroutine to get response
+		go func(i int, text string) {
+			res, err := GetResponse(prompt, text)
+			if err != nil {
+				fmt.Println("ERROR: ")
+				fmt.Println(err)
+				return
+			}
+			// send response and order number to channel
+			responseChan <- struct {
+				order    int
+				response string
+			}{i, res}
+		}(i, text)
+	}
+
+	// collect and sort responses
+	responses := make([]string, len(splitedText))
+	for i := 0; i < len(splitedText); i++ {
+		// receive response and order number from channel
+		response := <-responseChan
+		// store response in slice at the correct index
+		responses[response.order] = response.response
+	}
+
+	// print sorted responses
+	for _, res := range responses {
 		fmt.Println(res)
 	}
 }
